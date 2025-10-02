847184c0274d241403969fdee23a75cea314c449
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2025-10-02 20:29:08 -0300

    Feature: Criação de container base

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../angualar-material/angular-material.module";
+
+@NgModule({
+    declarations: [],
+    imports: [
+        AngularMaterialModule
+    ],
+    exports: [],
+})
+export class ComponentsModule { }
