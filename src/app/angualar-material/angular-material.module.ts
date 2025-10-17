f2903e27336d3d94890769722c642002ebba2985
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2025-10-16 21:30:55 -0300

    Feat: Criado o layout dos detalhes do usuário

--- src/app/angualar-material/angular-material.module.ts
+++ src/app/angualar-material/angular-material.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from "@angular/core";
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from "@angular/material/list";
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
     imports: [
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule
     ],
     exports: [
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule
     ],
 })
 export class AngularMaterialModule { }
