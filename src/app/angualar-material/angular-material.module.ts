8b743ad7abddab87e371742609c2ff32caa8e717
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2025-10-27 21:16:54 -0300

    Feat: Layout componente de filtro

--- src/app/angualar-material/angular-material.module.ts
+++ src/app/angualar-material/angular-material.module.ts
@@ -4,19 +4,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from "@angular/material/list";
 import { MatDividerModule } from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatIconModule,
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule
     ],
+    providers: [provideNativeDateAdapter()]
 })
 export class AngularMaterialModule { }
