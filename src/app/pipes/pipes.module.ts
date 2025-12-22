f5f4d5c86f6099b3ee32755248e4b12cea43bacb
Rodrigo Neves <rodrigo.emilio2004@gmail.com>
2025-12-21 22:00:13 -0300

    Feat: Criando pipes de Endereço e Status

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
+import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
     declarations: [
-    PhonePipe
+    PhonePipe,
+    AddressPipe,
+    StatusPipe
   ],
     exports: [
-        PhonePipe
+        PhonePipe,
+        AddressPipe,
+        StatusPipe
     ]
 })
 export class PipesModule {}
