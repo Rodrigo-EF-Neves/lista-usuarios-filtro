df55bcff1b6483109e7db5eafc60fc0014103a3c
Rodrigo Neves <rodrigo.emilio2004@gmail.com>
2026-01-10 15:28:04 -0300

    FeatureA Criando evento de click no usuário

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IUser } from '../../interfaces/User/user.interface';
 import { UsersList } from '../../data/users-list';
 
@@ -12,7 +12,9 @@ export class UsersListComponent {
   usersList: IUser[] = UsersList;
   displayedColumns: string[] = ['name', 'date', 'status'];
 
+  @Output('userSelected') userSelectedEmit = new EventEmitter<IUser>();
+
   onUserSelected(user: IUser): void {
-    console.log('Usuário selecionado:', user.nome);
+    this.userSelectedEmit.emit(user);
   }
 } 
